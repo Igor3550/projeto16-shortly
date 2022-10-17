a39ddab1715143bb2369576949943bd0d127d074
Igor3550 <igormeloigormelo@gmail.com>
2022-10-16 21:32:26 -0300

    feat/ranking: bugs: using LEFT JOIN in ranking query

--- src/controllers/rankingControllers.js
+++ src/controllers/rankingControllers.js
@@ -2,54 +2,34 @@ import db from '../database.js';
 
 async function getRanking (req, res) {
   try {
-    const usersWithout = await db.query(`
-      SELECT id, name FROM users WHERE users.id NOT IN (
-        SELECT user_id FROM shorts
-      );
-    `)
     const userShortsVisit = await db.query(`
       SELECT 
-        COUNT(short_visits.short_id) AS "visitCount",
+        users.id,
         users.name,
-        users.id
-      FROM short_visits
-      JOIN shorts ON shorts.id = short_visits.short_id
-      JOIN users ON users.id = shorts.user_id
-      GROUP BY users.id;
+        COUNT(short_visits.id) AS "visitCount"
+      FROM users
+      LEFT JOIN shorts ON shorts.user_id = users.id
+      LEFT JOIN short_visits ON shorts.id = short_visits.short_id
+      GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;
     `)
     const userShortsCount = await db.query(`
       SELECT 
-        COUNT(shorts.id) AS "linksCount",
         users.id,
-        users.name
-      FROM shorts
-      JOIN users ON users.id = shorts.user_id
-      GROUP BY users.id LIMIT 10;
+        users.name,
+        COUNT(shorts.id) AS "linksCount"
+      FROM users
+      LEFT JOIN shorts ON shorts.user_id = users.id
+      GROUP BY users.id ORDER BY "linksCount" DESC LIMIT 10;
     `)
 
-    const list = userShortsCount.rows.map((user) => {
-      const visitCounts = userShortsVisit.rows.filter(userVisits => userVisits.id === user.id)
+    const list = userShortsVisit.rows.map((user) => {
+      const linksList = userShortsCount.rows.filter(userLinks => userLinks.id === user.id)
       return {
         ...user,
-        visitCount: visitCounts[0] ? parseInt(visitCounts[0].visitCount) : 0
+        linksCount: linksList[0] ? linksList[0].linksCount : '0'
       }
     })
-    list.sort((a, b) => {
-      if(a.visitCount < b.visitCount) return 1
-      if(a.visitCount > b.visitCount) return -1
-      return 0
-    })
-    if(list.length < 10){
-      for(let i = 0; i<usersWithout.rows.length; i++){
-        if(list.length < 10){
-          list.push({
-            ...usersWithout.rows[i],
-            linksCount: 0,
-            visitCount: 0
-          })
-        }
-      }
-    }
+
     res.send(list)
   } catch (error) {
     console.log(error)
